803297693ee8e056ae5e88b8484f27b0814ea78a
MelonHell <MelonHell@ya.ru>
2022-10-16 03:47:28 +0300

    супер мега оптимизация

--- src/main/java/ru/melonhell/bulletphysics/ServerEventHandler.java
+++ src/main/java/ru/melonhell/bulletphysics/ServerEventHandler.java
@@ -1,13 +1,13 @@
 package ru.melonhell.bulletphysics;
 
-import com.destroystokyo.paper.event.server.ServerTickEndEvent;
-import com.destroystokyo.paper.event.server.ServerTickStartEvent;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 import ru.melonhell.bulletphysics.bullet.collision.space.MinecraftSpace;
 import ru.melonhell.bulletphysics.bullet.collision.space.generator.EntityCollisionGenerator;
 import ru.melonhell.bulletphysics.bullet.thread.PhysicsThread;
@@ -15,26 +15,34 @@ import ru.melonhell.bulletphysics.nms.wrappers.BlockPosWrapper;
 import ru.melonhell.bulletphysics.storage.SpaceStorage;
 import ru.spliterash.springspigot.listener.SpigotListener;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @SpigotListener
 @RequiredArgsConstructor
 public class ServerEventHandler implements Listener {
+    private final JavaPlugin javaPlugin;
     private final SpaceStorage spaceStorage;
     private final PhysicsThread physicsThread;
+    private BukkitTask bukkitTask;
 
-    @EventHandler
-    public void onServerTickStart(ServerTickStartEvent event) {
-        for (World world : Bukkit.getWorlds()) {
-            MinecraftSpace space = spaceStorage.get(world);
-            space.step();
-            EntityCollisionGenerator.step(space);
-        }
+    @PostConstruct
+    private void init() {
+        bukkitTask = Bukkit.getScheduler().runTaskTimerAsynchronously(javaPlugin, () -> {
+            for (World world : Bukkit.getWorlds()) {
+                MinecraftSpace space = spaceStorage.get(world);
+                space.step();
+                EntityCollisionGenerator.step(space);
+            }
+            if (physicsThread.throwable != null) {
+                throw new RuntimeException(physicsThread.throwable);
+            }
+        }, 0, 1);
     }
 
-    @EventHandler
-    public void onServerTickEnd(ServerTickEndEvent event) {
-        if (physicsThread.throwable != null) {
-            throw new RuntimeException(physicsThread.throwable);
-        }
+    @PreDestroy
+    private void destroy() {
+        bukkitTask.cancel();
     }
 
     @EventHandler
